b1361d3ff611377a8836f445b75863c6a9e8333a
Tetsuya Tada <tetsuya_tada@fandmnet.com>
2021-11-22 12:57:55 +0900

    まだ作りかけ(動かない) 【2021年8月最新】Providerを使って画面をPageとModelに分割しよう https://youtu.be/MvNyZrMr-3c 15:00 辺り

--- lib/book_list/book_list_model.dart
+++ lib/book_list/book_list_model.dart
@@ -6,14 +6,19 @@ class BookListModel extends ChangeNotifier {
   final Stream<QuerySnapshot> _usersStream =
   FirebaseFirestore.instance.collection('books').snapshots();
 
+  List<Book>? books; // Null許容
+
   void fetchBookList() {
     _usersStream.listen((QuerySnapshot snapshot) {
       final List<Book> books = snapshot.docs.map((DocumentSnapshot document) {
         Map<String, dynamic> data =
         document.data() as Map<String, dynamic>;
-
-
+        final String title = data['title'];
+        final String author = data['author'];
+        return Book(title, author);
       }).toList();
+      this.books = books;
+      notifyListeners();
     });
     
   }
--- lib/book_list/book_list_page.dart
+++ lib/book_list/book_list_page.dart
@@ -1,44 +1,35 @@
+import 'package:booklistsample/book_list/book_list_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class BookListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('本一覧'),
-      ),
-      body: Center(
-        child: StreamBuilder<QuerySnapshot>(
-          stream: _usersStream,
-          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            // if (snapshot.hasError) {
-            //   return Text('Something went wrong');
-            // }
-
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Text("Loading");
-            }
-
-            return ListView(
-              children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                Map<String, dynamic> data = document.data()! as Map<
-                    String,
-                    dynamic>;
-                return ListTile(
-                  title: Text(data['title']),
-                  subtitle: Text(data['author']),
-                );
-              }).toList(),
-            );
-          },
+    return ChangeNotifierProvider<BookListModel>(
+      create: (_) => BookListModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('本一覧'),
         ),
-      ),
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        body: Center(
+          child: Consumer<BookListModel>(builder: (context, model, child) {
+            final List<Book> books = model.books;
+            final List<Widget> widgets = books.map(
+                    (book) => ListTile(
+                      title: book.title,
+                      subtitle: book.author,
+                    ),
+                  );
+            return ListView.builder(itemBuilder: itemBuilder)
+          }),
+        ),
+        floatingActionButton: const FloatingActionButton(
+          onPressed: null,
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
