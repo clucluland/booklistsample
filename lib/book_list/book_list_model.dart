a55c8dacc6f06476017683bf96f07241d51577f5
Tetsuya Tada <tetsuya_tada@fandmnet.com>
2021-11-23 14:34:15 +0900

    【2021年8月最新】Providerを使って画面をPageとModelに分割しよう 一旦完了！

--- lib/book_list/book_list_model.dart
+++ lib/book_list/book_list_model.dart
@@ -3,13 +3,19 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
 class BookListModel extends ChangeNotifier {
+  // books が変化したら発火する(_usersStream)
   final Stream<QuerySnapshot> _usersStream =
   FirebaseFirestore.instance.collection('books').snapshots();
 
   List<Book>? books; // Null許容
 
   void fetchBookList() {
+    // 変化をリッスンする
+    // 変化があれば snapshot が変わる
     _usersStream.listen((QuerySnapshot snapshot) {
+      // mapで型変換
+      // DocumentSnapshot → book に変換、変換して配列に入れる
+      // title,authorを配列として取る
       final List<Book> books = snapshot.docs.map((DocumentSnapshot document) {
         Map<String, dynamic> data =
         document.data() as Map<String, dynamic>;
@@ -18,7 +24,7 @@ class BookListModel extends ChangeNotifier {
         return Book(title, author);
       }).toList();
       this.books = books;
-      notifyListeners();
+      notifyListeners();  // 呼び元で発火
     });
     
   }
--- lib/book_list/book_list_page.dart
+++ lib/book_list/book_list_page.dart
@@ -1,5 +1,5 @@
 import 'package:booklistsample/book_list/book_list_model.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:booklistsample/domain/book.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -8,21 +8,30 @@ class BookListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<BookListModel>(
-      create: (_) => BookListModel(),
+      create: (_) => BookListModel()..fetchBookList(),
       child: Scaffold(
         appBar: AppBar(
           title: const Text('本一覧'),
         ),
         body: Center(
           child: Consumer<BookListModel>(builder: (context, model, child) {
-            final List<Book> books = model.books;
+            final List<Book>? books = model.books;
+
+            if (books == null) {
+              // booksがnullのときぐるぐる待ち
+              // book_list_model の fetchBookList() で値が取れるまで null
+              return CircularProgressIndicator();
+            }
+            // ここより下は books は絶対 null でない
             final List<Widget> widgets = books.map(
                     (book) => ListTile(
-                      title: book.title,
-                      subtitle: book.author,
+                      title: Text(book.title),
+                      subtitle: Text(book.author),
                     ),
-                  );
-            return ListView.builder(itemBuilder: itemBuilder)
+                  ).toList();
+            return ListView(
+              children: widgets,
+            );
           }),
         ),
         floatingActionButton: const FloatingActionButton(
