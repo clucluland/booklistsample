be9e4ce81c136e6276138c681a78b31e1ac25f39
Tetsuya Tada <tetsuya_tada@fandmnet.com>
2021-12-03 12:38:19 +0900

    Left/Top をコメント化。Right/Bottom 飲み使う。  startActionPane: ActionPane() をコメント。

--- lib/book_list/book_list_page.dart
+++ lib/book_list/book_list_page.dart
@@ -29,33 +29,33 @@ class BookListPage extends StatelessWidget {
                   (book) => Slidable(
                     key: const ValueKey(0),
 
-                    // The start action pane is the one at the left or the top side.
-                    startActionPane: ActionPane(
-                      // A motion is a widget used to control how the pane animates.
-                      motion: const ScrollMotion(),
+                    // // The start action pane is the one at the left or the top side.
+                    // startActionPane: ActionPane(
+                    //   // A motion is a widget used to control how the pane animates.
+                    //   motion: const ScrollMotion(),
 
-                      // A pane can dismiss the Slidable.
-                      dismissible: DismissiblePane(onDismissed: () {}),
+                    //   // A pane can dismiss the Slidable.
+                    //   dismissible: DismissiblePane(onDismissed: () {}),
 
-                      // All actions are defined in the children parameter.
-                      children: const [
-                        // A SlidableAction can have an icon and/or a label.
-                        SlidableAction(
-                          onPressed: null,
-                          backgroundColor: Color(0xFFFE4A49),
-                          foregroundColor: Colors.white,
-                          icon: Icons.delete,
-                          label: 'Delete',
-                        ),
-                        SlidableAction(
-                          onPressed: null,
-                          backgroundColor: Color(0xFF21B7CA),
-                          foregroundColor: Colors.white,
-                          icon: Icons.share,
-                          label: 'Share',
-                        ),
-                      ],
-                    ),
+                    //   // All actions are defined in the children parameter.
+                    //   children: const [
+                    //     // A SlidableAction can have an icon and/or a label.
+                    //     SlidableAction(
+                    //       onPressed: null,
+                    //       backgroundColor: Color(0xFFFE4A49),
+                    //       foregroundColor: Colors.white,
+                    //       icon: Icons.delete,
+                    //       label: 'Delete',
+                    //     ),
+                    //     SlidableAction(
+                    //       onPressed: null,
+                    //       backgroundColor: Color(0xFF21B7CA),
+                    //       foregroundColor: Colors.white,
+                    //       icon: Icons.share,
+                    //       label: 'Share',
+                    //     ),
+                    //   ],
+                    // ),
                     // The end action pane is the one at the right or the bottom side.
                     endActionPane: const ActionPane(
                       motion: ScrollMotion(),
