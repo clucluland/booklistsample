05ce32133f21f5763c5fc01b42f3d370b183e57d
TETSUYA-PCK2\tetsuya <tetsuya_tada@fandmnet.com>
2022-01-09 18:57:53 +0900

    「本の追加」画面、イメージ部分タップで画像追加。 AndroidはOKだが、iOS設定が必要。 動画 19:00 より。

--- lib/add_book/add_book_model.dart
+++ lib/add_book/add_book_model.dart
@@ -1,12 +1,18 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddBookModel extends ChangeNotifier {
   String? title;
   String? author;
+  File? imageFile;
+
+  final picker = ImagePicker();
 
   // void → Future に変更
-  Future addBook() async{
+  Future addBook() async {
     if (title == null || title == '') {
       throw 'タイトルが入力されていません。';
     }
@@ -16,14 +22,22 @@ class AddBookModel extends ChangeNotifier {
     }
 
     // Firestore に登録
-    await FirebaseFirestore.instance.collection('books').add({
-      'title': title,
-      'author': author,
-    },);
+    await FirebaseFirestore.instance.collection('books').add(
+      {
+        'title': title,
+        'author': author,
+      },
+    );
 
     // title = null;
     // author = null;
-
   }
 
+  Future pickImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      imageFile = File(pickedFile.path);
+    }
+  }
 }
